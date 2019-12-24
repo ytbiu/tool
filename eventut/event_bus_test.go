aa7c0799ab597207d769a422d2eb43764a3324e8
尹涛 <yintao@exmarttech.com>
2019-12-24 15:53:44 +0800

    add test for event ut

---
+++ eventut/event_bus_test.go
@@ -0,0 +1,41 @@
+package eventut
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"sort"
+	"testing"
+	"time"
+)
+
+func TestPubSub(t *testing.T) {
+
+	topic := "test_topic"
+	eventMsg := "test-msg"
+	var msgGroup []string
+
+	PubSub().Sub(context.TODO(), topic, func(event interface{}) {
+		msg := event.(string)
+
+		fmt.Println("consumer 1 -- msg : ", msg)
+		msgGroup = append(msgGroup, msg+"1")
+
+	})
+
+	PubSub().Sub(context.TODO(), topic, func(event interface{}) {
+		msg := event.(string)
+		fmt.Println("consumer 2 -- msg : ", msg)
+		msgGroup = append(msgGroup, msg+"2")
+
+	})
+
+	PubSub().Pub(topic).Publish(eventMsg)
+
+	time.Sleep(2 * time.Second)
+
+	assert.Len(t, msgGroup, 2)
+	sort.Strings(msgGroup)
+	assert.True(t, reflect.DeepEqual(msgGroup, []string{eventMsg + "1", eventMsg + "2"}))
+}
